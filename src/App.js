44d01e6ca25299ea149f83fcbfa214b76614df74
Christian Parker <christianparker512@gmail.com>
2021-07-14 14:21:44 -0500

    expenses app in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+import expenses from './components/expenses.txt';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h2>Let's get started!</h2>
+        <ExpenseItem>
+            title={expenses[0].title}
+            amount{expenses[0].amount}
+            date={expenses[0].date}
+        </ExpenseItem>
+          <ExpenseItem>
+              title={expenses[1].title}
+              amount{expenses[1].amount}
+              date={expenses[1].date}
+          </ExpenseItem>
+          <ExpenseItem>
+              title={expenses[2].title}
+              amount{expenses[2].amount}
+              date={expenses[2].date}
+          </ExpenseItem>
+
+      </div>
   );
 }
 
